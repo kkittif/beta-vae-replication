4100aa9910489ab734d0cfb95b5edaa5291a0331
AlexH-T <73027427+AlexH-T@users.noreply.github.com>
2023-04-26 14:18:21 +0000

    testing arch on MNIST

---
+++ MNIST_data_training.py
@@ -0,0 +1,32 @@
+#%%
+import torch as t 
+from torchvision import datasets, transforms
+from torchvision.transforms import Compose, ToTensor, Resize
+import matplotlib.pyplot as plt
+
+from torch.utils.data import DataLoader
+
+
+MNIST_data = datasets.MNIST(
+    root='data',
+    train = True,
+    transform = Compose([ToTensor(), Resize((64, 64)),]),
+    download= True)
+
+# print(MNIST_data[0][0])
+
+#%% 
+plt.imshow(MNIST_data[0][0].reshape((64, 64, 1)))
+
+
+
+from arch import beta_VAE_chairs
+
+beta_VAE_MNIST = beta_VAE_chairs(k = 10)
+
+bernoulli_means = beta_VAE_MNIST(MNIST_data[0][0])
+
+# print(bernoulli_means)
+plt.imshow(bernoulli_means.detach().reshape(64, 64, 1))
+
+# %%
--- arch.py
+++ arch.py
@@ -3,28 +3,57 @@ import torch.nn as nn
 
 class beta_VAE_chairs(nn.Module):
     def __init__(self, k = 32):
-        super(self).__init__()
+        super(beta_VAE_chairs, self).__init__()
 
         self.latent_dim = k
-        self.encoder = nn.Sequential(
-            nn.Conv2d(1,32, kernel_size = (4,4), stride = 2), #input = 64x64x1 output = 31x31x32
-            nn.Conv2d(32,32, kernel_size = (4,4), stride = 2), #output = 14x14x32
-            nn.Conv2d(32,64, kernel_size = (4,4), stride = 2), #output = 6x6x64
-            nn.Conv2d(64,64, kernel_size = (4,4), stride = 2), #output = 2x2x64
-            nn.Flatten(),
+        self.encoder = nn.Sequential( 
+            nn.Conv2d(1,32, kernel_size = (4,4), stride = 2), #input = 1x64x64 output = 32x31x31
+            nn.Conv2d(32,32, kernel_size = (4,4), stride = 2), #output = 32x14x14
+            nn.Conv2d(32,64, kernel_size = (4,4), stride = 2), #output = 64x6x6
+            nn.Conv2d(64,64, kernel_size = (4,4), stride = 2), #output = 64x2x2
+            nn.Flatten(start_dim=0, end_dim=-1),
             nn.Linear(256,256),
             nn.ReLU(),
             nn.Linear(256, 2*k)
         )
 
 
+        self.decoder = nn.Sequential(
+        nn.Linear(k, 256), #output = 256
+        nn.Linear(256, 256), #output = 256
+        nn.Unflatten(dim=0, unflattened_size=(64, 2, 2)), #output = 64x2x2
+        nn.ConvTranspose2d(in_channels = 64, out_channels=64, kernel_size=(4,4), stride = 2), #output = 64x6x6
+        nn.ConvTranspose2d(in_channels = 64, out_channels=32, kernel_size=(4,4), stride = 2), #output = 32x14x14
+        nn.ConvTranspose2d(in_channels = 32, out_channels=32, kernel_size=(4,4), stride = 2, padding = 1), #output = 32x31x31 (without padding: 32x30x30)
+        nn.ConvTranspose2d(in_channels = 32, out_channels=1, kernel_size=(4,4), stride = 2),
+        nn.ReLU() #output should be 1x64x64, where each entry is a probability 
+        )
+
+        self.encoder_output = None
+        self.decoder_output = None
+
+    def reconstruct(self, sample: bool):
+        #Beta VAE paper seems to just take the mean. We could do that or sample from cont. Bernoulli
+        if sample:
+            return t.distributions.continuous_bernoulli.ContinuousBernoulli(probs = self.decoder_output)
+        else:
+            return self.decoder_output
 
     def forward(self, input):
-        encoder_output = self.encoder(input)
-        mu, sigma = t.split(encoder_output, self.latent_dim)
-        epsilon = t.normal(t.zeros(self.latent_dim), t.eye(self.latent_dim)) #TODO: doublecheck mean dimension
-        latent = mu + epsilon * sigma #k-dim vector
-        self.decoder(latent)
+        self.encoder_output = self.encoder(input)
+        mu, sigma = t.split(self.encoder_output, self.latent_dim)
+        Sampler = t.distributions.MultivariateNormal(t.zeros(self.latent_dim), t.eye(self.latent_dim))
+        epsilon = Sampler.sample()
+        # epsilon = t.normal(t.zeros(self.latent_dim), t.eye(self.latent_dim)) #TODO: doublecheck mean dimension
+        latent = mu + sigma * epsilon #k-dim vector
+        print(f"{mu=}")
+        print(f"{epsilon=}")
+        print(f"{sigma=}")
 
+        print(latent.shape)
+        print(latent)
+        self.decoder_output = self.decoder(latent)
+
+        return self.decoder_output
 
 
